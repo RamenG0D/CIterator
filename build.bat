560aba4f12dd196bcd3a935508512a77e21885f8
drplague1 <diebarian1203@yahoo.com>
2023-10-06 14:38:34 -0400

    fixed build script

--- build.bat
+++ build.bat
@@ -8,15 +8,11 @@ if "%1"=="" (
 )
 
 :: for compat with my other build scripts
-if "%1"=="" (
-  set LIB_NAME=CIterator
-) else (
-  set LIB_NAME=%~2
-)
+set LIB_NAME=CIterator
 
 echo Compiling %LIB_NAME%
 
-%COMPILER% -c ./src/%LIB_NAME%.c -O2 -I./includes -o ./%LIB_NAME%.o
+%COMPILER% -c -w ./src/%LIB_NAME%.c -O2 -I./includes -o ./%LIB_NAME%.o
 
 ar -rvs lib%LIB_NAME%.a ./%LIB_NAME%.o
 
